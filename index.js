f71056855bae28e9aa24c68121552e62b03afa25
beto-dt <lucho.mix34@gmail.com>
2020-07-20 19:14:25 -0500

    project finish

--- index.js
+++ index.js
@@ -4,9 +4,23 @@ const { db, http, signs }= require("./util");
 
 class Main {
     static async getHoroscope(){
+       const todayHoroscope = [];
+       for( const sign of signs ) {
+             const { horoscope }  = (await http.get('/' + sign)).data;
+             todayHoroscope.push({
+                 sign,
+                 horoscope
+             });
+       }
 
+       const name = new Date()
+       .toDateString()
+       .split(" ")
+       .join("_");
+
+       db.setItem(name, JSON.stringify(todayHoroscope));
     }
 }
- cron.schedule('0 0 * * * *', () => {
+ cron.schedule('* * * * * *', () => {
    Main.getHoroscope(); 
 }); 
--- util/axios.js
+++ util/axios.js
@@ -1,4 +1,4 @@
 const axios = require('axios').default;
 module.exports = axios.create({
-    baseURL="http://horoscope-api.herokuapp.com/horoscope/today/"
+    baseURL: "http://horoscope-api.herokuapp.com/horoscope/today/"
 })
